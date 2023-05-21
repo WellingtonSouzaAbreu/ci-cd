171745110422d61f63c81efadd6d99240cf9b9c6
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-05-21 08:52:43 -0400

    feat: Implements initial ESLint config

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,20 @@
-import { View, StatusBar } from 'react-native';
+/* eslint-disable import/no-default-export */
+import React from 'react'
+import {
+	View, StatusBar, TouchableOpacity, Text,
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View >
-      <StatusBar backgroundColor={'transparent'} barStyle='dark-content' />
-    </View>
-  )
+	return (
+		<View>
+			<StatusBar
+				backgroundColor={'transparent'}
+				barStyle={'dark-content'}
+			/>
+			<TouchableOpacity>
+				<View />
+				<Text>{'This is a text'}</Text>
+			</TouchableOpacity>
+		</View>
+	)
 }
