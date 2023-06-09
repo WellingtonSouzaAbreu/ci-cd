9a279d3576b3f97ced0ad4c62bc12efa3d0b1036
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-06-08 21:42:03 -0400

    feat(auth): Implements local authentication function

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable import/no-default-export */
 import React from 'react'
-import { View, StatusBar, Text } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
 export default function App() {
 	return (
 		<View>
 			<StatusBar backgroundColor={'transparent'} />
-			<Text>{'This is a text'}</Text>
 		</View>
 	)
 }
