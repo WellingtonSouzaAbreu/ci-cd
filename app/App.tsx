313bc0309c908777ab958f36af29a8ee1d3829d0
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-08-12 09:41:24 -0400

    feat(context): Implements context loader modal

--- app/App.tsx
+++ app/App.tsx
@@ -6,6 +6,8 @@ import React from 'react'
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 
+import { LoaderProvider } from '@contexts/LoaderContext/index'
+
 import { StartupStack } from '@routes/stacks/StartupStack'
 
 import { defaultTheme } from '@presentation/common/theme'
@@ -14,8 +16,10 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<ThemeProvider theme={defaultTheme}>
-				<StatusBar backgroundColor={'green'} />
-				<StartupStack />
+				<LoaderProvider>
+					<StatusBar backgroundColor={'green'} />
+					<StartupStack />
+				</LoaderProvider>
 			</ThemeProvider>
 		</NavigationContainer>
 	)
---
+++ app/src/presentation/components/animated/Loader/index.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+
+import LottieView from 'lottie-react-native'
+
+import { AnimationContainer, Container } from './styles'
+import verticalBars from '@presentation/assets/animatios/test.json'
+// buildingColorPassingLight
+
+interface LoaderProps {
+	flex?: boolean
+	animationScale?: number
+}
+
+function Loader({ ...props }: LoaderProps) {
+	const animation = useRef<any>(null)
+
+	return (
+		<Container flex={props.flex}>
+			<AnimationContainer animationScale={props.animationScale}>
+				<LottieView
+					source={verticalBars}
+					ref={animation}
+					autoPlay
+					loop
+					speed={1.5}
+				/>
+			</AnimationContainer>
+		</Container>
+	)
+}
+
+Loader.defaultProps = {
+	flex: false,
+	animationScale: 85
+}
+
+export { Loader }
