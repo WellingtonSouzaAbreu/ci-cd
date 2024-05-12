e293ec8b858e4ed55515d581e07b678ff953a5b5
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-05-12 18:07:48 -0400

    fix: Corrige erro de login no android

--- app/src/presentation/components/animated/Loader/index.tsx
+++ app/src/presentation/components/animated/Loader/index.tsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react'
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { useTheme } from 'styled-components'
 
-import verticalBars from '@assets/animatios/verticalBars.json'
-import LottieView from 'lottie-react-native'
+// import verticalBars from '@assets/animatios/verticalBars.json'
+// import LottieView from 'lottie-react-native'
 
 import { AnimationContainer, Container } from './styles'
 // buildingColorPassingLight
@@ -12,18 +14,21 @@ interface LoaderProps {
 }
 
 function Loader({ ...props }: LoaderProps) {
-	const animation = useRef<any>(null)
+	// const animation = useRef<any>(null)
+
+	const theme = useTheme()
 
 	return (
 		<Container flex={props.flex}>
 			<AnimationContainer animationScale={props.animationScale}>
-				<LottieView
+				{/* <LottieView
 					source={verticalBars}
 					ref={animation}
 					autoPlay
 					loop
 					speed={1.5}
-				/>
+				/> */}
+				<ActivityIndicator size={'large'} color={theme.green2} />
 			</AnimationContainer>
 		</Container>
 	)
