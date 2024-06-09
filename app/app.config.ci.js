fb250f546bc0782030bf248065980e11d0cfe8f0
Wellington Souza Abreu <64331839+WellingtonSouzaAbreu@users.noreply.github.com>
2024-06-08 20:30:59 -0400

    feat: commitando certo na test/someting

--- app/app.config.ci.js
+++ app/app.config.ci.js
@@ -1,4 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('./app.config.js')
 
-module.exports = { ...config.default.expo }
+module.exports = {
+	...config.default.expo,
+	generateBuild: {
+		android: true,
+		ios: false
+	}
+}
