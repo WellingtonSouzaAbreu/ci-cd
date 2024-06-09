23378dadf1c833a37c7515fc649943412ef6423b
Wellington Souza Abreu <64331839+WellingtonSouzaAbreu@users.noreply.github.com>
2024-06-09 19:38:37 -0400

    feat: commitando certo na test/someting

--- app/src/contexts/AuthContext/index.tsx
+++ app/src/contexts/AuthContext/index.tsx
@@ -1,10 +1,6 @@
-import React, { createContext, useContext, useEffect } from 'react'
-import { Alert } from 'react-native'
-
-import { useFirebaseConfig } from '@config/firebase/useFirebaseConfig'
+import React, { createContext, useContext } from 'react'
 
 import { UserEntity } from '@domain/user/model/entity/types'
-import { useUserDomain } from '@domain/user/useUserDomain'
 
 import { AuthContextType } from './types'
 
@@ -24,68 +20,6 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextType>(initialValue)
 
 function AuthProvider({ children }: AuthProviderProps) {
-	// const { showContextModal } = useAlertContext()
-
-	// const [userRegistrationData, setUserRegisterDataContext] = useState<UserRegisterData>()
-	// const [userAuthData, setUserAuthDataContext] = useState<UserAuthData>({})
-	// const [authenticatedUser, setAuthenticatedUser] = useState<AuthenticatedUserDate>()
-
-	// const { navigateToAuthScreen, navigateToQuickLogin, navigateToHome } = useAuthNavigation()
-
-	useEffect(() => {
-		Alert.alert('Iniciou o effect')
-		/* console.log('Sessão inciada!')
-		const unsubscribe = firebaseAuth.onAuthStateChanged(async (user) => {
-			console.log(user ? 'Usuário logado!' : 'Usuário não logado!')
-			if (user && await hasValidLocalUser()) {
-				const { requestDevicePasswordOnAuth } = await UserUseCases.getUserPreferences(UserLocalRepository)
-				console.log('requestDevicePasswordOnAuth =>', requestDevicePasswordOnAuth)
-
-				return requestDevicePasswordOnAuth
-					? navigateToQuickLogin()
-					: performQuickSingin()
-			}
-			return navigateToAuthScreen()
-		})
-
-		return unsubscribe */
-	}, [])
-
-	// const performQuickSingin = async () => { // Quick signin virar um caso de uso
-	// 	try {
-	// 		const userId = firebaseAuth.currentUser.uid
-	// 		const user = await UserUseCases.performQuickSignin(UserRemoteRepository, UserLocalRepository, userId)
-	// 		setUserDataOnContext(user)
-	// 		navigateToHome()
-	// 	} catch (error) {
-	// 		console.log(error)
-	// 		showContextModal('', 'Houve um erro ao tentar recuperar suas informações!')
-	// 		navigateToAuthScreen()
-	// 	}
-	// }
-
-	// const setUserRegisterDataOnContext = (data: UserRegisterData) => {
-	// 	setUserRegisterDataContext({ ...userRegistrationData, ...data })
-	// }
-
-	// const setUserAuthDataOnContext = (data: UserAuthData) => {
-	// 	setUserAuthDataContext({ ...userAuthData, ...data })
-	// }
-
-	// const setUserDataOnContext = (data: AuthenticatedUserDate) => {
-	// 	setAuthenticatedUser({ ...authenticatedUser, ...data })
-	// }
-
-	// const authProviderData = useMemo(() => ({
-	// 	userRegistrationData,
-	// 	setUserRegisterDataOnContext,
-	// 	userAuthData,
-	// 	setUserAuthDataOnContext,
-	// 	authenticatedUser,
-	// 	setUserDataOnContext
-
-	// }), [userRegistrationData, userAuthData, authenticatedUser])
-
 	return (
 		<AuthContext.Provider value={'' as any}>
 			{children}
