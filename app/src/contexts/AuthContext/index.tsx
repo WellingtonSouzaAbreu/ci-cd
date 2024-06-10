c2d56c5ab34a06c150ffa57c1e9fe5b4d6a180d4
Wellington Souza Abreu <64331839+WellingtonSouzaAbreu@users.noreply.github.com>
2024-06-09 21:57:16 -0400

    feat: commitando certo na test/someting

--- app/src/contexts/AuthContext/index.tsx
+++ app/src/contexts/AuthContext/index.tsx
@@ -45,20 +45,20 @@ function AuthProvider({ children }: AuthProviderProps) {
 
 	useEffect(() => {
 		Alert.alert('vars', env)
-		// const unsubscribe = firebaseAuth.onAuthStateChanged(async (user) => {
-		// 	console.log(user ? 'Usuário logado!' : 'Usuário não logado!')
-		// 	if (user /* && await hasValidLocalUser() */) {
-		// 		const { requestDevicePasswordOnAuth } = await UserUseCases.getUserPreferences(UserLocalRepository)
-		// 		console.log('requestDevicePasswordOnAuth =>', requestDevicePasswordOnAuth)
+		const unsubscribe = firebaseAuth.onAuthStateChanged(async (user) => {
+			console.log(user ? 'Usuário logado!' : 'Usuário não logado!')
+			if (user /* && await hasValidLocalUser() */) {
+				const { requestDevicePasswordOnAuth } = await UserUseCases.getUserPreferences(UserLocalRepository)
+				console.log('requestDevicePasswordOnAuth =>', requestDevicePasswordOnAuth)
 
-		// 		return requestDevicePasswordOnAuth
-		// 			? navigateToQuickLogin()
-		// 			: performQuickSingin()
-		// 	}
-		// 	return navigateToAuthScreen()
-		// })
+				return requestDevicePasswordOnAuth
+					? navigateToQuickLogin()
+					: performQuickSingin()
+			}
+			return navigateToAuthScreen()
+		})
 
-		// return unsubscribe
+		return unsubscribe
 	}, [])
 
 	const performQuickSingin = async () => { // Quick signin virar um caso de uso
